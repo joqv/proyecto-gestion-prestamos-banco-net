c56137e6942eddd5e5ae1e81626d5834c76b7ad4
isaMirka <quispesicham@gmail.com>
2025-08-30 01:33:53 -0500

    cambios del login con jwt completado

---
+++ GestionPrestamosBancoService/Data/Respositorio/UsuarioRepository.cs
@@ -0,0 +1,82 @@
+﻿using GestionPrestamosBancoService.Data.Contrato;
+using GestionPrestamosBancoService.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace GestionPrestamosBancoService.Data.Respositorio
+{
+    public class UsuarioRepository:IUsuario
+    {
+        private readonly IConfiguration config;
+        private readonly string cadenaConexion;
+
+        public UsuarioRepository(IConfiguration configuration)
+        {
+            config = configuration;
+            cadenaConexion = config["ConnectionStrings:DB"];
+        }
+
+        public Usuario Login(string username, string password)
+        {
+            Usuario usuario = null;
+
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (var cmd = new SqlCommand("LoginUsuario", conexion))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader != null && reader.HasRows)
+                        {
+                            reader.Read();
+                            usuario = ConvertirReaderEnObjeto(reader);
+                        }
+                    }
+                }
+            }
+
+            return usuario;
+        }
+
+        public Usuario ObtenerPorUsername(string username)
+        {
+            Usuario usuario = null;
+
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (var cmd = new SqlCommand("ObtenerUsuarioPorUsername", conexion))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", username);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader != null && reader.HasRows)
+                        {
+                            reader.Read();
+                            usuario = ConvertirReaderEnObjeto(reader);
+                        }
+                    }
+                }
+            }
+
+            return usuario;
+        }
+        private Usuario ConvertirReaderEnObjeto(SqlDataReader reader)
+        {
+            return new Usuario()
+            {
+                IdUsuario = reader.GetInt32(reader.GetOrdinal("IdUsuario")),
+                Username = reader.GetString(reader.GetOrdinal("Username")),
+                PasswordHash = reader.GetString(reader.GetOrdinal("PasswordHash")),
+                IdCliente = reader.GetInt32(reader.GetOrdinal("IdCliente"))
+            };
+        }
+    }
+}
--- GestionPrestamosBancoService/Program.cs
+++ GestionPrestamosBancoService/Program.cs
@@ -1,6 +1,13 @@
 using GestionPrestamosBancoService.Data.Contrato;
 using GestionPrestamosBancoService.Data.Respositorio;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -18,6 +25,43 @@ builder.Services.AddScoped<ICuotaPrestamo, CuotaPrestamoRepositorio>();
 builder.Services.AddScoped<ICuentaBancaria, CuentaBancariaRepositorio>();
 builder.Services.AddScoped<IMoneda, MonedaRepositorio>();
 builder.Services.AddScoped<ITipoCuenta, TipoCuentaRepositorio>();
+builder.Services.AddScoped<IUsuario, UsuarioRepository>();
+
+// Clave token llamado
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+
+// Configurar autenticación JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+       /* options.Events = new JwtBearerEvents
+        {
+            OnChallenge = async context =>
+            {
+                context.HandleResponse();
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/json";
+
+                var result = System.Text.Json.JsonSerializer.Serialize(new
+                {
+                    message = "Necesita logearse o faltan permisos"
+                });
+
+                await context.Response.WriteAsync("[]");
+            }
+        };*/
+    });
+
+builder.Services.AddAuthorization();
+
 
 var app = builder.Build();
 
@@ -29,6 +73,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.UseAuthorization();
 
